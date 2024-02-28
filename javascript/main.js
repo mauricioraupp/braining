61013430d788fd2bdccc9a3244cb2f4b2e149feb
Mauricio <mauricio.p.raupp@gmail.com>
2024-02-28 11:52:08 -0300

    progresso em aula

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,11 @@ function scrollToDiv(div) {
       div.scrollIntoView({ behavior: 'smooth' });
     }
   }
+
+var mediaQuery = window.matchMedia("(max-width: 710px)");
+const novoUl = document.querySelector(".novoUl")
+const ignore = novoUl.children[4];
+
+if (mediaQuery.matches){
+  ignore.remove();
+}
