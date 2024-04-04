574c5905df494018e5c5f6f264a67709cb458cc1
Mauricio <mauricio.p.raupp@gmail.com>
2024-04-04 20:46:51 -0300

    sistema do memory game finalizado

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ function scrollToDiv(div) {
   }
 }
 
+const buttonFirst = document.querySelector('.first-button');
+const buttonNav = document.querySelector('.minijogos-first-button');
+
+buttonFirst.addEventListener('click', function() {
+  window.location.href = '../html/minigames.html';
+});
+buttonNav.addEventListener('click', function() {
+  window.location.href = '../html/minigames.html';
+});
+
 var mediaQuery = window.matchMedia("(max-width: 768px)");
 const novoUl = document.querySelector(".novoUl");
 const ignore = novoUl.children[4];
+const ignore2 = novoUl.children[5];
 
 if (mediaQuery.matches){
 ignore.remove();
+ignore2.remove();
 }
--- js/minigames.js
+++ js/minigames.js
@@ -1,7 +1,25 @@
-var mediaQuery = window.matchMedia("(max-width: 768px)");
-const novoUl = document.querySelector(".novoUl");
+var mediaQuery = window.matchMedia('(max-width: 768px)');
+const novoUl = document.querySelector('.novoUl');
 const ignore = novoUl.children[4];
+const ignore2 = novoUl.children[5];
 
 if (mediaQuery.matches){
 ignore.remove();
+ignore2.remove();
 }
+
+const memoryGame = document.querySelector('#art-1');
+const memoryGamePopup = document.querySelector('.memory-game-popup');
+const closePopup = document.querySelector('#close-popup');
+
+memoryGame.addEventListener('click', function() {
+  setTimeout(() => {
+    memoryGamePopup.style.opacity = '1';
+    memoryGamePopup.style.zIndex = '1';
+  },100)
+});
+
+closePopup.addEventListener('click', function() {
+  memoryGamePopup.style.opacity = '0';
+  memoryGamePopup.style.zIndex = '-1';
+})
