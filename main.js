7abb75bf64d0f628601366e3d778ea8f901869c6
Mauricio <mauricio.p.raupp@gmail.com>
2024-06-05 00:19:38 -0300

    mobile menu

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ buttonNav.addEventListener('click', function() {
   window.location.href = 'html/minigames.html';
 });
 
+
+
 let prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
@@ -20,3 +22,25 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
+const navObjects = {
+  desktopNav: document.querySelector(".desktop-nav"),
+  mobileNav: document.querySelector(".mobile-nav"),
+  mobileMenu: document.querySelector(".mobile-menu"),
+  openMenu: document.querySelector("#open-menu"),
+  closeMenu: document.querySelector("#close-menu")
+}
+
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+if (mediaQuery.matches) {
+  navObjects.desktopNav.classList.toggle("hide");
+  navObjects.mobileNav.classList.toggle("hide");
+}
+
+navObjects.openMenu.addEventListener('click', function() {
+  navObjects.mobileMenu.classList.remove("hide")
+})
+
+navObjects.closeMenu.addEventListener('click', function() {
+  navObjects.mobileMenu.classList.add("hide")
+})
