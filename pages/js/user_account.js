4e4539ee4f208d0579d94783f2db8eb3bcc465c1
Mauricio <mauricio.p.raupp@gmail.com>
2024-08-15 11:43:33 -0300

    name get and put

--- pages/js/user_account.js
+++ pages/js/user_account.js
@@ -1,11 +1,13 @@
 const buttonNav = document.querySelector('nav ul div button');
-const storedAccount = localStorage.getItem('@account_logged');
 
 buttonNav.addEventListener('click', function() {
   window.location.href = '../pages/minigames.html';
 });
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  const storedAccount = localStorage.getItem('@account_logged');
   
   if (storedAccount) {
     const account = JSON.parse(storedAccount);
@@ -17,28 +19,22 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-const logoutButton = document.querySelector("#button-sair")
+
+
+const logoutButton = document.querySelector("#button-sair");
 
 logoutButton.addEventListener('click', function(){
-  if (confirm("Tem certeza que deseja sair da conta?") == true){
-    try {
-      if (storedAccount){
-        localStorage.removeItem('@account_logged')
-        alert("Conta desconectada")
-        window.location.href = '.././index.html'
-      } else {
-        throw new Error()
-      }
-    } catch(err) {
-      alert("Nenhuma conta logada")
+  if (confirm("Tem certeza que deseja sair da conta?")) {
+    const storedAccount = localStorage.getItem('@account_logged');
+    if (storedAccount){
+      localStorage.removeItem('@account_logged');
+      alert("Conta desconectada");
+      window.location.href = '../index.html';
+    } else {
+      alert("Nenhuma conta logada");
     }
   }
 });
-
-
-
-
-
 
 
 
@@ -47,55 +43,60 @@ const svg = document.querySelector(".lucide-x");
 
 svg.addEventListener('click', function(){
   container.style.display = 'none';
-})
+});
+
+
 
 const buttonName = document.getElementById("name-change");
-const buttonDate = document.getElementById("date-change");
-const buttonEmail = document.getElementById("email-change");
-const buttonPassword = document.getElementById("password-change");
 
-buttonName.onclick = async function(event) {
+buttonName.onclick = function(event) {
   event.preventDefault();
-  container.style.display = 'flex';
-
-  document.querySelector("#button-avancar").addEventListener('click', async function(){
-    try {
-      const account = JSON.parse(storedAccount);
-      let email = account.email;
-
-      const response = await fetch(`http://localhost:3003/api/store/userrequest?email=${email}`, {
-        method: "GET",
-        headers: { "Content-type": "application/json;charset=UTF-8" },
-      });
-
-      let content = await response.json();
-      console.log(content.data[0])
-
-      if (content.success) {
-        if (document.querySelector("#senha").value == content.data[0]){
-
-          let name = document.querySelector("#input-name").value
-          
-          const response = await fetch(`http://localhost:3003/api/store/userupdate?name=${name}&email=${email}`, {
+  const storedAccount = localStorage.getItem('@account_logged');
+  const accountParse = JSON.parse(storedAccount);
+  if (document.querySelector("#input-name").value == accountParse.name){
+    alert("Nenhuma alteração identificada")
+  } else {
+    container.style.display = 'flex';
+  
+    const buttonAvancar = document.querySelector("#button-avancar");
+    
+    buttonAvancar.onclick = async function() {
+      try {
+        const storedAccount = localStorage.getItem('@account_logged');
+        const account = JSON.parse(storedAccount);
+        let email = account.email;
+        let password = document.getElementById("senha").value;
+  
+        const response = await fetch(`http://localhost:3003/api/store/userrequest?email=${email}&password=${password}`, {
+          method: "GET",
+          headers: { "Content-type": "application/json;charset=UTF-8" },
+        });
+  
+        let content = await response.json();
+  
+        if (content.success) {
+          let name = document.querySelector("#input-name").value;
+  
+          const updateResponse = await fetch(`http://localhost:3003/api/store/userupdate?name=${name}&email=${email}`, {
             method: "PUT",
             headers: { "Content-type": "application/json;charset=UTF-8" },
           });
-
-          let content = await response.json();
-
-          if(content.success){
-            let account = content.data;
-            alert(content.message);
+  
+          let updateContent = await updateResponse.json();
+  
+          if(updateContent.success){
+            alert(updateContent.message);
+            account.name = name;
             localStorage.setItem('@account_logged', JSON.stringify(account));
           } else {
-            alert(content.message)
+            alert(updateContent.message);
           }
-        } 
-      } else {
-        alert(content.message);
+        } else {
+          alert(content.message);
+        }
+      } catch (error) {
+        alert('Falha ao conectar com o servidor.');
       }
-    } catch (error) {
-      alert('Falha ao conectar com o servidor.');
     }
-  })
+  }
 }
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,8 @@
 const connection = require('../config/db');
 const dotenv = require('dotenv').config();
+const bcrypt = require('bcrypt');
 
 async function userRequest(request, response) {
-  try {
 
     const params = Array(
       request.query.email,
@@ -10,33 +10,50 @@ async function userRequest(request, response) {
 
     const query = 'SELECT password FROM user_account WHERE email = ? ';
 
-    connection.query(query, params, (err, results) => {
-      if (results) {
-        console.log(query, params, results)
-        response
-          .status(201)
-          .json({
-            success: true,
-            message: 'select sucesso',
-            data: results
-          });
-      } else {
-        response
-          .status(400)
-          .json({
-            success: false,
-            message: 'select errado',
-            data: err
-          });
+    connection.query(query, params, async (err, results) => {
+      if (err) {
+        return response.status(500).json({
+          success: false,
+          message: 'Erro no servidor',
+          data: err
+        });
       }
-    });
-  } catch (error) {
-    response.status(500).json({
-      success: false,
-      message: 'Erro no servidor',
-      data: error
-    });
-  }
+  
+      if (results.length > 0) {
+        const account = results[0];
+        const resultPassword = account.password;
+        const formPassword = request.query.password;
+
+  
+        try {
+          const match = await bcrypt.compare(formPassword, resultPassword);
+          console.log(resultPassword, formPassword, match)
+          if (match) {
+            return response.status(200).json({
+              success: true,
+              message: 'Senha correta',
+              data: account
+            });
+          } else {
+            return response.status(400).json({
+              success: false,
+              message: 'Senha inválida'
+            });
+          }
+        } catch (error) {
+          return response.status(500).json({
+            success: false,
+            message: 'Erro ao comparar senhas',
+            data: error
+          });
+        }
+      } else {
+        return response.status(400).json({
+          success: false,
+          message: 'Email inválido'
+        });
+      }
+    })
 }
 
 async function userUpdate(request, response) {
