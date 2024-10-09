7ca52fa605329323f6e08c3ab27b4a106ff42f12
mauricioraupp <mauricio.p.raupp@gmail.com>
2024-10-08 22:46:30 -0300

    footer js

--- pages/js/footer.js
+++ pages/js/footer.js
@@ -1,14 +1,37 @@
+let instagramSvg;
+let githubSvg;
+let linkedinSvg;
+let emailSvg;
+
+(function checkDocument() {
+  if (document.title === "Braining - Página inicial") {
+    instagramSvg = "assets/instagram.svg"
+    githubSvg = "assets/github.svg"
+    linkedinSvg = "assets/linkedin.svg"
+    emailSvg = "assets/mail.svg"
+  } else {
+    instagramSvg = "../assets/instagram.svg"
+    githubSvg = "../assets/github.svg"
+    linkedinSvg = "../assets/linkedin.svg"
+    emailSvg = "../assets/mail.svg"
+  }
+})();
+
 document.querySelector("#footer-root").innerHTML = `
-<footer>
+  <footer>
     <article>
-      <a href="https://www.instagra.com/mauricioraupp_"><img src="assets/instagram.svg" alt="Logo do instagram"></a>
-      <a href="https://github.com/mauricioraupp"><img src="assets/github.svg" alt="Logo do github"></a>
-      <a href="https://www.linkedin.com/in/mauricioraupp1/"><img src="assets/linkedin.svg" alt="Logo do linkedin"></a>
-      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui" target="blank" rel="noopener noreferrer"><img src="assets/mail.svg" alt="Logo do email"></a>
+      <a href="https://www.instagram.com/mauricioraupp_" 
+      target="blank" rel="noopener noreferrer"><img src="${instagramSvg}" alt="Logo do instagram"></a>
+      <a href="https://github.com/mauricioraupp" 
+      target="blank" rel="noopener noreferrer"><img src="${githubSvg}" alt="Logo do github"></a>
+      <a href="https://www.linkedin.com/in/mauricioraupp1/" 
+      target="blank" rel="noopener noreferrer"><img src="${linkedinSvg}" alt="Logo do linkedin"></a>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui"
+      target="blank" rel="noopener noreferrer"><img src="${emailSvg}" alt="Logo do email"></a>
     </article>
     <div>
       <a href="#">Sobre</a>
-      <a href="#">Contato</a>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui">Contato</a>
       <a href="#">FAQs</a>
       <a href="#">Carreira</a>
       <a href="#">Blog</a>
--- pages/js/nav.js
+++ pages/js/nav.js
@@ -12,46 +12,46 @@ let minigamesHref;
 })();
 
 document.querySelector("#nav-root").innerHTML = `
-<nav>
-<ul class="desktop-nav">
-  <div>
-    <a href="../index.html"><img src="../assets/whiterockettext.png" alt="Logo do site"></a>
-    <a href="../index.html">Home</a>
-    <a href="#">Sobre</a>
-    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui">Contato</a>
-  </div>
-  <div>
-    <a id="nav-item" href="${loginHref}">Login</a>
-    <button>Minijogos</button>
-  </div>
-</ul>
+  <nav>
+    <ul class="desktop-nav">
+      <div>
+        <a href="../index.html"><img src="../assets/whiterockettext.png" alt="Logo do site"></a>
+        <a href="../index.html">Home</a>
+        <a href="#">Sobre</a>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui">Contato</a>
+      </div>
+      <div>
+        <a id="nav-item" href="${loginHref}">Login</a>
+        <button>Minijogos</button>
+      </div>
+    </ul>
 
-<ul class="mobile-nav hide">
-  <img id="open-menu" src="../assets/align-justify.svg">
-  <div>
-    <a href="#">Sobre</a>
-    <a id="nav-item" href="${loginHref}">Login</a>
-  </div>
-</ul>
-</nav>
+    <ul class="mobile-nav hide">
+      <img id="open-menu" src="../assets/align-justify.svg">
+      <div>
+        <a href="#">Sobre</a>
+        <a id="nav-item" href="${loginHref}">Login</a>
+      </div>
+    </ul>
+  </nav>
 
-<div class="mobile-menu hide">
-<ul>
-  <article class="user">
-    <img id="user-img">
-    <div class="user-wrapper">
-      <p id="user-name">Nenhuma conta conectada</p>
-    </div>
-    <a class="hide" id="nav-item" href="${loginHref}">Login</a>
-  </article>
-  <a id="nav-item" href="${loginHref}">Login</a>
-  <a href="../index.html">Home</a>
-  <a href="${minigamesHref}">Minijogos</a>
-  <a href="#">Sobre</a>
-  <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui">Contato</a>
-</ul>
-<img id="close-menu" src="../assets/x.svg">
-</div>
+  <div class="mobile-menu hide">
+    <ul>
+      <article class="user">
+        <img id="user-img">
+        <div class="user-wrapper">
+          <p id="user-name">Nenhuma conta conectada</p>
+        </div>
+        <a class="hide" id="nav-item" href="${loginHref}">Login</a>
+      </article>
+      <a id="nav-item" href="${loginHref}">Login</a>
+      <a href="../index.html">Home</a>
+      <a href="${minigamesHref}">Minijogos</a>
+      <a href="#">Sobre</a>
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=mauricio.p.raupp@gmail.com&su=Assunto%20do%20email&body=Escreva%20sua%20mensagem%20aqui">Contato</a>
+    </ul>
+    <img id="close-menu" src="../assets/x.svg">
+  </div>
 `
 
 const storedAccount2 = localStorage.getItem('@account_logged');
